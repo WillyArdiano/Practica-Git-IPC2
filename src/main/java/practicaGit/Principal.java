e565fd13502d82e71abf88bc3f353bc1890ab6d9
Astrid Martinez <astrid_martinez60@yahoo.es>
2019-07-27 11:25:26 -0600

    primer modelo de interface

---
+++ src/main/java/practicaGit/Principal.java
@@ -0,0 +1,11 @@
+
+package practicaGit;
+import practicagit.UI.VentanaPrincipal;
+public class Principal {
+    
+    public static void main(String[] args) {
+       VentanaPrincipal nueva = new VentanaPrincipal();
+       nueva.setVisible(true);
+        
+    }
+}
