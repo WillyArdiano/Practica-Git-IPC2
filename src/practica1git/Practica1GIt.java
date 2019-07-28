caca61e6b5f613c59192ae390273579fe6045caa
Astrid Martinez <astrid_martinez60@yahoo.es>
2019-07-27 18:41:55 -0600

    cambios1

---
+++ src/practica1git/Practica1GIt.java
@@ -0,0 +1,12 @@
+
+package practica1git;
+import practica1git.UI.VentanaPrincipal;
+
+public class Practica1GIt {
+    public static void main(String[] args) {
+      
+       VentanaPrincipal nueva = new VentanaPrincipal();
+       nueva.setVisible(true);  
+    }
+    
+}
--- src/practica1git/UI/VentanaPrincipal.java
+++ src/practica1git/UI/VentanaPrincipal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package practicagit.UI;
+package practica1git.UI;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
